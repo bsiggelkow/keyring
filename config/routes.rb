b030b9afb5f78afb6100498939195e663776ba2b
Bill Siggelkow <bsiggelkow@mac.com>
2008-10-31 09:36:00 -0400

    Switched to use jRails

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
   map.resources :keywords, :collection => { :clear_all => :delete }
-  map.resources :main, :controller => 'main'
+  map.resources :main, :controller => 'main', :collection => {:search => :get}
 
   # The priority is based upon order of creation: first created -> highest priority.
 
@@ -41,6 +41,6 @@ ActionController::Routing::Routes.draw do |map|
   # Install the default routes as the lowest priority.
   # Note: These default routes make all actions in every controller accessible via GET requests. You should
   # consider removing the them or commenting them out if you're using named routes and resources.
-  map.connect ':controller/:action/:id'
-  map.connect ':controller/:action/:id.:format'
+  # map.connect ':controller/:action/:id'
+  # map.connect ':controller/:action/:id.:format'
 end
